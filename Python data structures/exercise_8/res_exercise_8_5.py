6741af6b7e75b12b16b5062aa3a9be499e584ffb
Vanessa1507 <lauritasuarezg@gmail.com>
2023-02-14 17:08:06 -0500

    Feat: Answer exercise 8.5

---
+++ Python data structures/exercise_8/res_exercise_8_5.py
@@ -0,0 +1,10 @@
+han = open("../../txt/mbox-short.txt")
+
+for line in han:
+  line = line.rstrip()
+  wds = line.split()
+
+  #Guardian
+  if len(wds) < 3  or wds[0] != "From ":
+  
+  print(wds[2])
