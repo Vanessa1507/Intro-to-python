0e240e53528bce04de9c7c8b514ff8cd7570f3a2
Vanessa1507 <lauritasuarezg@gmail.com>
2023-02-14 15:37:03 -0500

    Clean cache

--- Python data structures/exercise_7/res_exercise_7_1.py
+++ Python data structures/exercise_7/res_exercise_7_1.py
@@ -3,3 +3,7 @@ file = open("./mbox-short.txt")
 for line in file :
   newLine = line.rstrip()
   print(newLine.upper())
+
+abc = "First; Second; Third"
+split = abc.split("; ")
+print(split) #['with', 'three', 'words']
