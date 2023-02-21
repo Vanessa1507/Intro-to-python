72b1fa3603cf72999070fd10b38eebb9e56bf2f8
Vanessa1507 <lauritasuarezg@gmail.com>
2023-02-21 17:09:41 -0500

    Tweak: beautifulSoap file: Ignore SSL certificate errors

--- using-python-to-access-web-data/week4/beautifulSoupTest.py
+++ using-python-to-access-web-data/week4/beautifulSoupTest.py
@@ -1,9 +1,15 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 # http://www.dr-chuck.com/page2.htm
 url = input("Enter - ")
-html = urllib.request.urlopen(url).read() # Read all
+html = urllib.request.urlopen(url, context=ctx).read() # Read all
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
--- using-python-to-access-web-data/week4/unicodeCharacters_Strings.py
+++ using-python-to-access-web-data/week4/unicodeCharacters_Strings.py
@@ -2,6 +2,7 @@ print("Representing Simple strings")
 print(ord("H")) # 72
 print(ord("e")) # 101
 print(ord("\n")) # 10
+print(ord("i")) # 10
 
 print("\nTwo kinds of strings in Python")
 x = b"abc"
